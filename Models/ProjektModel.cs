138c9e8dc7af062a252bb114a840f7e998bcc232
David Bekeris <david-_-bkrs@hotmail.com>
2023-12-29 15:53:38 +0100

    Projekt model

---
+++ Models/ProjektModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DavidBekeris.Models
+{
+    public class ProjektModel
+    {
+        public string picture { get; set; }
+        public string header { get; set; }
+        public string description { get; set; }
+        public string link { get; set; }
+    }
+}
