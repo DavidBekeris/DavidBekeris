4101a81e63cb14cb032c53eceba8f1c0fe413e5d
David Bekeris <david-_-bkrs@hotmail.com>
2024-03-16 12:09:55 +0100

    Models done

---
+++ Data/Entities/IndexModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DavidBekeris.Data.Entities
+{
+    public class IndexModel
+    {
+        public IndexModel(int id, Uri heroUrl, string heroText, string heroHeader)
+        {
+            Id = id;
+            this.heroUrl = heroUrl;
+            this.heroText = heroText;
+            this.heroHeader = heroHeader;
+        }
+
+        public int Id { get; set; }
+        public Uri heroUrl { get; protected set; }
+        public string heroText { get; set; }
+        public string heroHeader { get; set; }
+    }
+}
---
+++ Data/Entities/ProjektModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace DavidBekeris.Data.Entities
+{
+    public class ProjektModel
+    {
+        public ProjektModel(int id, Uri imageUrl, string header, string description, string projectLink)
+        {
+            Id = id;
+            ImageUrl = imageUrl;
+            Header = header;
+            Description = description;
+            ProjectLink = projectLink;
+        }
+
+        public int Id { get; set; }
+        public Uri ImageUrl { get; protected set; }
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public string ProjectLink { get; set; }
+        public string UrlSlug { get; protected set; }
+
+    }
+}
