6ad84fb3d643482c5e5b9d339acbbaca49f9b061
DavidBekeris <david-_-bkrs@hotmail.com>
2025-06-17 15:03:22 +0200

    Added download limit by ip for resume

---
+++ wwwroot/js/resume-download.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("resume-download").addEventListener("click", downloadResume);
+});
+
+async function downloadResume(event) {
+    event.preventDefault();
+
+    const response = await fetch('/download-resume');
+
+    if (response.status === 429) {
+        const message = await response.text();
+        alert(message);
+        return;
+    }
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'David Bekeris CV - Webbutvecklare.pdf';
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("resume-download").addEventListener("click", downloadResume);
+});
