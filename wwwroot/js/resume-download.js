7ab220e08cbf6d9c108c3422d118241158de5694
DavidBekeris <david-_-bkrs@hotmail.com>
2025-06-18 08:43:45 +0200

    Added google analytics and resume download tracker

--- wwwroot/js/resume-download.js
+++ wwwroot/js/resume-download.js
@@ -5,6 +5,16 @@
 async function downloadResume(event) {
     event.preventDefault();
 
+
+    // 🔍 Google Analytics 4 tracking
+    if (typeof gtag === "function") {
+        gtag('event', 'cv_download', {
+            event_category: 'Nedladdning',
+            event_label: 'CV - Webbutvecklare',
+            value: 1
+        });
+    }
+
     const response = await fetch('/download-resume');
 
     if (response.status === 429) {
