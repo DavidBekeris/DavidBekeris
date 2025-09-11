adb706e16ff61ed553018f71bf34559b684a3c92
DavidBekeris <david-_-bkrs@hotmail.com>
2025-09-11 19:38:10 +0200

    Adding recaptcha as first step towards contact form anti spam

---
+++ Services/GoogleRecaptchaHelper.cs
@@ -0,0 +1,29 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System.Text.Json;
+
+namespace DavidBekeris.Services
+{
+    public class GoogleRecaptchaHelper
+    {
+        private readonly IAmazonSecretsManager _secretsManager;
+
+        public GoogleRecaptchaHelper(string region)
+        {
+            _secretsManager = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+        }
+
+        public async Task<string> GetSecretKeyAsync()
+        {
+            var request = new GetSecretValueRequest
+            {
+                SecretId = "GoogleReCaptcha-Secret"
+            };
+
+            var response = await _secretsManager.GetSecretValueAsync(request);
+            using var doc = JsonDocument.Parse(response.SecretString);
+            return doc.RootElement.GetProperty("RECAPTCHA_SECRET").GetString();
+        }
+    }
+}
